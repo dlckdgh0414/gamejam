0beb897e6b23bb7d5611550d57fc40738c03566b
dlckdgh0414 <ci5976547@gmail.com>
2024-07-29 17:45:44 +0900

    feat : DeadEvent

--- Assets/01_Scripts/Lch/Enemy.cs
+++ Assets/01_Scripts/Lch/Enemy.cs
@@ -6,12 +6,17 @@ public enum EnemyEnum
 {
     Idle,
     Walk,
-    faint
+    Dead
 }
 
-public class Enemy : EnemyAgent
+public class Enemy : EnemySetting
 {
     public StateMachine<EnemyEnum> StateMachine { get; private set; }
+
+    public override void SetDeadState()
+    {
+        StateMachine.ChangeState(EnemyEnum.Dead);
+    }
 
     public int EnemyCount;
     protected override void Awake()
@@ -21,8 +26,8 @@ public class Enemy : EnemyAgent
         StateMachine = new StateMachine<EnemyEnum>();
 
         StateMachine.AddState(EnemyEnum.Idle, new EnemyIdle(this, StateMachine, "Idle"));
-        StateMachine.AddState(EnemyEnum.Walk, new EnemyWalk(this, StateMachine, "Idle"));
-        StateMachine.AddState(EnemyEnum.faint, new EnemyFaint(this, StateMachine, "Idle"));
+        StateMachine.AddState(EnemyEnum.Walk, new EnemyWalk(this, StateMachine, "Walk"));
+        StateMachine.AddState(EnemyEnum.Dead, new EnemyFaint(this, StateMachine, "Dead"));
 
     }
 
