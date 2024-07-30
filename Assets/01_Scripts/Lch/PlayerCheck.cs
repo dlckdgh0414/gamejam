56979e1e25feb0e42f91e5df0f70750862a03b82
dlckdgh0414 <ci5976547@gmail.com>
2024-07-31 04:09:16 +0900

    fix : Enemy Attack

--- Assets/01_Scripts/Lch/PlayerCheck.cs
+++ Assets/01_Scripts/Lch/PlayerCheck.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerCheck : MonoBehaviour
 {
     public EnemySetting _enemy;
+
+    public UnityEvent PlayerDaedEvent;
 
     private float _dlay;
 
