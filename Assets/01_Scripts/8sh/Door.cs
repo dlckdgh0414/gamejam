2fcbe2bf39d4bdf223bfbf7190d99d268e23d166
8Sehyun <8Sehyun@gmail.com>
2024-07-31 06:54:03 +0900

    added door

--- Assets/01_Scripts/8sh/Door.cs
+++ Assets/01_Scripts/8sh/Door.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private bool canOpen = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Player"))
+        {
+            canOpen = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        
+        canOpen = false;
     }
 }
