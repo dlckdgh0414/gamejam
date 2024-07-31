f9fd226d050d74b1700edcb682c0f48ab79a0bc8
dlckdgh0414 <ci5976547@gmail.com>
2024-07-31 11:33:51 +0900

    feat : Test

---
+++ Assets/01_Scripts/Lch/KeyCardTest.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+
+public class KeyCardTest : MonoBehaviour
+{
+    public static KeyCardTest instance;
+
+    [SerializeField] private GameObject keycard;
+    [SerializeField] private TMP_Text alarm;
+
+
+    public bool _keycard;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void getkeycard()
+    {
+        if (_keycard == false)
+        {
+            alarm.DOFade(1, 1);
+            keycard.SetActive(true);
+            _keycard = true;
+            alarm.DOFade(0, 1).SetDelay(2);
+        }
+        else
+        {
+
+        }
+    }
+
+    public void minuskeycard()
+    {
+        if (_keycard == true)
+        {
+            keycard.SetActive(false);
+            _keycard = false;
+
+        }
+    }
+}
